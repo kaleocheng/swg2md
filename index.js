41501bb4608464cfcc5ab56694a4f31a1762f8b1
Kaleo <kaleocheng@gmail.com>
2019-04-18 21:32:19 +0800

    Refactor render function

--- index.js
+++ index.js
@@ -1,123 +1,110 @@
 #!/usr/bin/env node
 
 const SwaggerParser = require('swagger-parser')
-const Swagmock = require('swagmock')
 const Mustache = require('mustache')
 const fs = require('fs')
 
-function getRequireBody(name, obj, result) {
+
+function getFlatJSONSchema(name, jsonSchema, flatedJSONSchema) {
+    let result = []
+    flatJSONSchema(name, jsonSchema, flatedJSONSchema)
+    if (!Array.isArray(flatedJSONSchema[0])) {
+        result.push(flatedJSONSchema)
+    } else {
+        result = flatedJSONSchema
+    }
+    return result
+}
+
+function flatJSONSchema(name, jsonSchema, flatedJSONSchema) {
     const nameset = new Set([])
-    const ofKeys = ['oneOf', 'anyOf', 'allOf']
-    for (let key of ofKeys) {
-        if (key in obj) {
-            for (let o of obj[key]) {
+    const schemaOfKeys = ['oneOf', 'anyOf', 'allOf']
+    for (let key of schemaOfKeys) {
+        if (key in jsonSchema) {
+            for (let body of jsonSchema[key]) {
                 let r = []
-                getRequireBody('', o, r)
-                result.push(r)
+                flatJSONSchema('', body, r)
+                flatedJSONSchema.push(r)
             }
         }
     }
 
-    if (!('type' in obj)) {
-        obj.type = 'object'
+    if (!('type' in jsonSchema)) {
+        jsonSchema.type = 'object'
     }
 
     if (!nameset.has(name) && name != '') {
-        result.push({
+        flatedJSONSchema.push({
             name: name,
-            description: obj.description || name,
-            type: obj.format || obj.type
+            description: jsonSchema.description || name,
+            type: jsonSchema.format || jsonSchema.type
         })
         nameset.add(name)
     }
 
-    switch (obj.type) {
+    switch (jsonSchema.type) {
         case 'object':
-            if ('properties' in obj) {
-                for (let n of Object.keys(obj.properties)) {
+            if ('properties' in jsonSchema) {
+                for (let n of Object.keys(jsonSchema.properties)) {
                     let newName = (name == '') ? n : `${name}.${n}`
-                    getRequireBody(newName, obj.properties[n], result)
+                    flatJSONSchema(newName, jsonSchema.properties[n], flatedJSONSchema)
                 }
             }
             break
         case 'array':
-            if ('items' in obj) {
-                getRequireBody(`${name}[item]`, obj.items, result)
+            if ('items' in jsonSchema) {
+                flatJSONSchema(`${name}[item]`, jsonSchema.items, flatedJSONSchema)
             }
             break
     }
-
 }
 
-async function getItems(api) {
+function flatRequestBody(requestBody) {
+    const flatedJSONSchema = []
+    const jsonSchema = requestBody.content && requestBody.content['application/json'] && requestBody.content['application/json'].schema || []
+    getFlatJSONSchema('', jsonSchema, flatedJSONSchema)
+    requestBody.flatedJSONSchema = flatedJSONSchema
+    return requestBody
+}
+
+function flatResponses(responses) {
+    const fr = []
+    for (let code of Object.keys(responses)) {
+        let flatedJSONSchema = []
+        let r = responses[code]
+        r.code = code
+        let jsonSchema = responses[code].content && responses[code].content['application/json'] && responses[code].content['application/json'].schema || []
+        getFlatJSONSchema('', jsonSchema, flatedJSONSchema)
+        r.responseBody = flatedJSONSchema
+        fr.push(r)
+    }
+    return fr
+}
+
+function transForMD(api) {
     const items = []
+
     const paths = Object.keys(api.paths)
-    let mockgen = Swagmock(api, {
-        validated: true
-    })
     for (let p of paths) {
         const methods = Object.keys(api.paths[p])
         for (let m of methods) {
-            const item = {}
-            const apiItem = api.paths[p][m]
+            const item = api.paths[p][m]
             item.title = `${m} ${p}`
-            item.seq = `${m}${p.replace(/\//g, '-')}`
             item.path = p
             item.method = m
-            item.description = apiItem.description
-            item.parameters = apiItem.parameters || []
-            item.requestbody = apiItem.requestBody && apiItem.requestBody.content && apiItem.requestBody.content['application/json'] && apiItem.requestBody.content['application/json'].schema || []
-            item.responses = []
-            for (let code of Object.keys(apiItem.responses)) {
-                let r = apiItem.responses[code]
-                r.code = code
-                r.responsebody = apiItem.responses[code] && apiItem.responses[code].content && apiItem.responses[code].content['application/json'] && apiItem.responses[code].content['application/json'].schema || []
-                item.responses.push(r)
-            }
-
+            item.methodUpperCase = m.toUpperCase()
+            item.flatedRequestBody = flatRequestBody(item.requestBody || {})
+            item.flatedResponses = flatResponses(item.responses || {})
             items.push(item)
         }
     }
-
-
-    for (let item of items) {
-        let bodyItems = []
-        getRequireBody('', item.requestbody, bodyItems)
-        if (!Array.isArray(bodyItems[0])) {
-            item.requestbody = []
-            item.requestbody.push(bodyItems)
-        } else {
-            item.requestbody = bodyItems
-        }
-
-
-        for (let r of item.responses) {
-            let bodyItems = []
-            getRequireBody('', r.responsebody, bodyItems)
-            if (!Array.isArray(bodyItems[0])) {
-                r.responsebody = []
-                r.responsebody.push(bodyItems)
-            } else {
-                r.responsebody = bodyItems
-            }
-            r.responseMock = []
-            mock = await mockgen.responses({
-                path: item.path,
-                operation: item.method,
-                response: r.code
-            })
-            if (mock) {
-                r.responseMock.push(JSON.stringify(mock.responses, null, 2))
-            }
-        }
-
-    }
-    return items
+    api.flatPaths = items
+    return api
 }
 
 async function render(templateFile, swaggerFile) {
     const api = await SwaggerParser.validate(swaggerFile)
-    const items = await getItems(api)
+    const items = await transForMD(api)
     const template = fs.readFileSync(templateFile, 'utf8')
     return Mustache.render(template, items);
 }
